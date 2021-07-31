9cd20259189ceb991ba6b71d4d4a8dcb444f94cd
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-31 12:54:54 +0300

    Move unit tests for separate file to plotspc.R

--- R/plotspc.R
+++ R/plotspc.R
@@ -566,7 +566,25 @@ plotspc <- function(object,
 
 # Unit tests -----------------------------------------------------------------
 
-# TODO: add unit tests here
+hySpc.testthat::test(plotspc) <- function() {
+
+  context("plotspc")
+
+  test_that("BARBITURATES are plotted", {
+    expect_silent(
+      spc <- do.call(collapse, barbiturates[1:3])
+    )
+
+    expect_silent(
+      plotspc(
+        spc,
+        col = palette_matlab_dark(3),
+        stacked = TRUE,
+        lines.args = list(type = "h")
+      )
+    )
+  })
+}
 
 
 # Function -------------------------------------------------------------------
