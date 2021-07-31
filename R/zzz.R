abf575abab351f4bfaf6f99d606a8d994c219250
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-31 13:37:22 +0300

    Apply `styler`

--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,6 @@
   packageStartupMessage(
     "\n\n",
     "Package ", desc$Package, " (version ", desc$Version, ")\n\n",
-
     "To get started, try: \n",
     '   vignette("', desc$Package, '", package = "', desc$Package, '")', "\n",
     "   package?", desc$Package, "\n",
@@ -15,11 +14,9 @@
     '   vignette(package = "', desc$Package, '")', "\n",
     # '   browseURL("', first_url, '") # Online documentation \n',
     "\n",
-
     "If you use this package, please cite it appropriately.\n",
     "The correct reference is given by:\n",
     '   citation("', desc$Package, '")\n\n',
-
     "The project's website:\n   ", first_url, "\n\n",
     sep = ""
   )
