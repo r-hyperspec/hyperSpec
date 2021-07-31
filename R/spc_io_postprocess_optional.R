84601c814d818f307adb66881753f79b69155f0e
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-31 13:08:08 +0300

    Update style

--- R/spc_io_postprocess_optional.R
+++ R/spc_io_postprocess_optional.R
@@ -1,4 +1,6 @@
-#' Helper Function to Harmonize Treatment of File Import Results
+# Function -------------------------------------------------------------------
+
+#' Helper function to harmonize treatment of file import results
 #'
 #' This function provides two ways of post-processing imported spectra:
 #'
@@ -6,20 +8,20 @@
 #'   empty spectra when measurements are cancelled)
 #' - optionally keep the filenames in column `spc$filename`
 #'
-#' The desired overall behavior can be set by options via [hy.setOptions()]. All
-#' file import filters should call `.spc_io_postprocess_optional()` to ensure the same
-#' behavior.
+#' The desired overall behavior can be set by options via [hy.setOptions()].
+#' All file import filters should call `.spc_io_postprocess_optional()` to
+#' ensure the same behavior.
 #'
-#' @param spc hyperSpec object for file import post-processing
-#' @param filename filename(s) to become extra data column of `spc`
-#' @param ... (ignored)
+#' @param spc `hyperSpec` object for file import post-processing.
+#' @param filename filename(s) to become extra data column of `spc`.
+#' @param ... (ignored).
 #' @param file.remove.emptyspc should empty (all `NA` or all `0`) spectra be
 #'        removed?
 #' @param file.keep.name should file names be kept and put into `spc$filename`?
 #' @param tolerance intensities in +/- `tolerance` are considered `0` for
-#'        `file.remove.emptyspc = TRUE`
+#'        `file.remove.emptyspc = TRUE`.
 #'
-#' @return hyperSpec object
+#' @return `hyperSpec` object.
 #'
 #' @keywords internal
 #'
@@ -29,9 +31,9 @@
 #' @export
 #'
 .spc_io_postprocess_optional <- function(spc, filename, ...,
-                             file.remove.emptyspc = hy.getOption("file.remove.emptyspc"),
-                             file.keep.name = hy.getOption("file.keep.name"),
-                             tolerance = hy.getOption("tolerance")) {
+                                         file.remove.emptyspc = hy.getOption("file.remove.emptyspc"),
+                                         file.keep.name = hy.getOption("file.keep.name"),
+                                         tolerance = hy.getOption("tolerance")) {
   tolerance <- .checkpos(tolerance, "tolerance")
 
   if (file.remove.emptyspc) {
@@ -61,24 +63,37 @@
 }
 
 
+# Unit tests -----------------------------------------------------------------
+
 hySpc.testthat::test(.spc_io_postprocess_optional) <- function() {
   context(".spc_io_postprocess_optional")
 
   test_that("removing of zero/NA spectra", {
     tmp <- fluNA # spectrum 2 is all NA
     tmp[[3]] <- 0
-    tmp[[5]] <- runif(nwl(tmp), min = -hy.getOption("tolerance") / 2, max = hy.getOption("tolerance") / 2)
+    tmp[[5]] <- runif(nwl(tmp),
+      min = -hy.getOption("tolerance") / 2,
+      max = hy.getOption("tolerance") / 2
+    )
     tmp[[, , 450 ~ 455]] <- NA
 
     expect_equivalent(
-      .spc_io_postprocess_optional(tmp, file.remove.emptyspc = TRUE, file.keep.name = FALSE),
-      tmp [-c(2, 3, 5)]
+      .spc_io_postprocess_optional(tmp,
+        file.remove.emptyspc = TRUE,
+        file.keep.name = FALSE
+      ),
+      tmp[-c(2, 3, 5)]
     )
   })
 
   test_that("filenames", {
     flu$filename <- NULL
-    tmp <- .spc_io_postprocess_optional(flu, filename = "test", file.remove.emptyspc = FALSE, file.keep.name = TRUE)
+    tmp <- .spc_io_postprocess_optional(
+      flu,
+      filename = "test",
+      file.remove.emptyspc = FALSE,
+      file.keep.name = TRUE
+    )
     expect_true(all(tmp$filename == "test"))
 
     expect_warning(tmp <- .spc_io_postprocess_optional(tmp,
