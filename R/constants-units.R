bf524debe1f45967adce278d6410afefbb135928
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-08-11 23:13:07 +0300

    Update constants-units.R

--- R/constants-units.R
+++ R/constants-units.R
@@ -1,4 +1,8 @@
-# @concept constants
+# Expressions with `wl` units
+#
+# @rdname wl-units
+# @docType data
+# @concept wl
 units_wavenumber_shift <- expression(Delta * tilde(nu) / cm^-1)
 units_wavenumber <- expression(tilde(nu) / cm^-1)
 units_wavelength <- expression(lambda / nm)
