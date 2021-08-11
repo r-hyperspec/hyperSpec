c0aa2342f10711a8c12afaf8ef1d1dcb96fea4ca
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-08-11 23:07:50 +0300

    Rename unexported variables
    Related to #52

--- R/constants-units.R
+++ R/constants-units.R
@@ -1,4 +1,4 @@
 # @concept constants
-.wn_shift <- expression(Delta * tilde(nu) / cm^-1)
-.wn <- expression(tilde(nu) / cm^-1)
-.wl <- expression(lambda / nm)
+units_wavenumber_shift <- expression(Delta * tilde(nu) / cm^-1)
+units_wavenumber <- expression(tilde(nu) / cm^-1)
+units_wavelength <- expression(lambda / nm)
