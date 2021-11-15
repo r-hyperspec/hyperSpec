986cf592bc862f253e3731f047a6fef89adbdd5a
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-11-15 17:57:51 +0200

    Improve unit tests and documentation

--- R/wl_convert_units.R
+++ R/wl_convert_units.R
@@ -106,6 +106,24 @@ wl_raman2nm    <- function(x, ref_wl) 1e7 / (1e7 / ref_wl - x)
 #' can be used as a standardized argument value for other \pkg{hyperSpec}
 #' functions, e.g., [wl_convert_units()].
 #'
+#'
+#' @param unit (sting):
+#'        A name, abbreviation or shot description of unit for wavelength axis.
+#'
+#' @param null_ok (logical):
+#'        If `unit = NULL` and `null_ok = TRUE`, the function will not fail and
+#'        will return `NULL`.
+#'
+#' @param on_failure (string):
+#'        The type of behavior in case unrecognized value of `unit` is passed
+#'        (i.e., in case of failure to convert to a standardized value):
+#' - `"fail"` -- the code is stopped with an error message.
+#' - `"warn"` -- a warning is issued and the value of `unit` is returned as
+#'               an output.
+#' - `"pass"` -- the value of `unit` is returned as an output and no error nor
+#'               warning is issued.
+#'
+#'
 #' @return
 #' The function returns one of these "standard" values:
 #' - `"raman"` for Raman shift in relative 1/cm;
@@ -116,13 +134,8 @@ wl_raman2nm    <- function(x, ref_wl) 1e7 / (1e7 / ref_wl - x)
 #' - `"px"`    for pixels.
 #'
 #'
-#' @param on_failure (string)
-#'        The type of behavior in case unrecognized value of `unit` is passed:
-#' - `"fail"` -- the code is stopped with an error message.
-#' - `"warn"` -- a warning is issued and the value of `unit` is returned as
-#'               an output.
-#' - `"pass"` -- the value of `unit` is returned as an output and no error nor
-#'               warning is issued.
+#' @seealso [wl_convert_units()]
+#' @author R. Kiselev, V. Gegzna
 #'
 #' @export
 #'
@@ -229,13 +242,22 @@ hySpc.testthat::test(.wl_fix_unit_name) <- function() {
 
   test_that(".wl_fix_unit_name() works", {
     expect_equal(.wl_fix_unit_name("raman"), "raman")
+    expect_equal(.wl_fix_unit_name("raman shift"), "raman")
+    expect_equal(.wl_fix_unit_name("raman_shift"), "raman")
+    expect_equal(.wl_fix_unit_name("raman.shift"), "raman")
     expect_equal(.wl_fix_unit_name("invcm"), "invcm")
     expect_equal(.wl_fix_unit_name("nm"), "nm")
     expect_equal(.wl_fix_unit_name("ev"), "ev")
     expect_equal(.wl_fix_unit_name("freq"), "freq")
     expect_equal(.wl_fix_unit_name("px"), "px")
     expect_equal(.wl_fix_unit_name("file"), "file")
-    expect_error(.wl_fix_unit_name("ddd"), "Unknown unit type")
+
+    expect_error(.wl_fix_unit_name("dDd"), "Unknown unit type")
+    expect_equal(.wl_fix_unit_name("dDd", on_failure = "pass"), "dDd")
+    expect_warning(.wl_fix_unit_name("dDd", on_failure = "warn"), "dDd")
+
+    expect_error(.wl_fix_unit_name(NULL), "argument is of length zero")
+    expect_silent(.wl_fix_unit_name(NULL, null_ok = TRUE))
   })
 
   # TODO (tests): add more specific tests.
