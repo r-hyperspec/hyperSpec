b72f75e5148def30d86a6103ec8b681239abfe3f
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-11-15 21:21:19 +0200

    Delete constants-units.R
    ...as it is not used elsewhere

--- R/constants-units.R
+++
@@ -1,8 +0,0 @@
-# Expressions with `wl` units
-#
-# @rdname wl-units
-# @docType data
-# @concept wl
-units_wavenumber_shift <- expression(Delta * tilde(nu) / cm^-1)
-units_wavenumber <- expression(tilde(nu) / cm^-1)
-units_wavelength <- expression(lambda / nm)
