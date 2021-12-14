a541313be83bbfc1bfc31e234b2605be2e268910
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-12-14 21:31:59 +0200

    Create unit tests

--- R/cov_pooled.R
+++ R/cov_pooled.R
@@ -69,6 +69,64 @@ cov_pooled <- function(x, groups, ..., regularize = 1e-5 * max(abs(COV))) {
   )
 }
 
-# Function -------------------------------------------------------------------
+# Unit tests -----------------------------------------------------------------
 
-# FIXME: add unit tests
+hySpc.testthat::test(.cov_h_) <- function() {
+  context("Covariance")
+
+  n_wl <- nwl(faux_cell)
+  obj <- cov(faux_cell)
+
+  # Properties
+  expect_true(is.matrix(obj))
+  expect_type(obj, "double")
+  expect_equal(dim(obj), c(n_wl, n_wl))
+
+  # Contents
+  expect_equivalent(obj[1, 1],   cov(faux_cell[[, ,  1, wl.index = TRUE]]))
+  expect_equivalent(obj[9, 9],   cov(faux_cell[[, ,  9, wl.index = TRUE]]))
+  expect_equivalent(obj[20, 20], cov(faux_cell[[, , 20, wl.index = TRUE]]))
+  expect_equivalent(obj[50, 50], cov(faux_cell[[, , 50, wl.index = TRUE]]))
+}
+
+
+hySpc.testthat::test(cov_pooled) <- function() {
+  context("Pooled covariance")
+
+  obj <- cov_pooled(faux_cell, faux_cell$region)
+
+  n_wl <- nwl(faux_cell)
+  n_means <- length(unique(faux_cell$region))
+
+  expect_true(is.list(obj))
+  expect_equal(names(obj), c("COV", "means"))
+
+  # $means
+  expect_s4_class(obj$means, "hyperSpec")
+  expect_equal(nrow(obj$means), n_means)
+  expect_equal(nwl(obj$means), n_wl)
+
+  expect_equivalent(
+    tapply(faux_cell$spc[, 1], faux_cell$region, mean),
+    obj$means$spc[, 1]
+  )
+  expect_equivalent(
+    tapply(faux_cell$spc[, 9], faux_cell$region, mean),
+    obj$means$spc[, 9]
+  )
+  expect_equivalent(
+    tapply(faux_cell$spc[, 20], faux_cell$region, mean),
+    obj$means$spc[, 20]
+  )
+  expect_equivalent(
+    tapply(faux_cell$spc[, 50], faux_cell$region, mean),
+    obj$means$spc[, 50]
+  )
+
+  # $COV
+  expect_true(is.matrix(obj$COV))
+  expect_equal(dim(obj$COV), c(n_wl, n_wl))
+
+  # FIXME: the contents of covariance matrix must be tested
+
+}
