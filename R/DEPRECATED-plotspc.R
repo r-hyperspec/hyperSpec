81f9022a1f915e87f8601f7523ccce86201ac680
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-12-15 01:36:52 +0200

    Deprecate plotspc

---
+++ R/DEPRECATED-plotspc.R
@@ -0,0 +1,35 @@
+#' @name DEPRECATED-plotspc
+#' @concept deprecated
+#'
+#' @title (DEPRECATED)
+#'        Plotting spectra
+#'
+#' @description
+#' These \pkg{hyperSpec} functions are **deprecated** and not maintained any
+#' more. You should not use these.
+#' Currently they are present due to back-compatibility reasons and will be
+#' removed in the next release of the package.
+#' Please, use the suggested alternative functions instead:
+#'
+#' - [hyperSpec::plot_spc()]
+#'
+#'
+#' @param ... arguments to [hyperSpec::plot_spc()].
+#'
+#' @include raster.R
+#' @export
+plotspc <- function(...) {
+  hySpc_deprecated("plot_spc")
+  plot_spc(...)
+}
+
+
+# Unit tests -----------------------------------------------------------------
+
+hySpc.testthat::test(plotspc) <- function() {
+  context("Deprecated functions")
+
+  test_that("plotspc() is deprecated", {
+    expect_error(expect_warning(plot_spc(), "deprecated"))
+  })
+}
